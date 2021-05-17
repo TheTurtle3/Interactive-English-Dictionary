0b282e135bec404ae07965534d56d28c31522cae
Ahmad Zafar <62062766+TheTurtle3@users.noreply.github.com>
2021-05-17 11:50:22 -0400

    Working version 1.0

---
+++ app.py
@@ -0,0 +1,36 @@
+import json
+from difflib import get_close_matches
+
+# creating a dict out of the data.json file
+data = json.load(open("data.json"))
+
+# function that finds the def of word
+def meaning(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        maybe = get_close_matches(w, data.keys())[0]
+        yn = input("Did you mean %s instead? (Y/N) " % maybe).lower()
+        if yn == "y":
+            return data[maybe]
+        elif yn == "n":
+            return "The word does not exist in the Dictionary!"
+        else:
+            return "We didn't unserstand what you entered."
+    else:
+        return "The word does not exist in the Dictionary!"
+
+# get user word and print its def
+while True:
+    user_input = input("Enter a word: ")
+    output = meaning(user_input)
+    
+    if type(output) == list:
+        for item in output:
+            print(item)
+    else:
+        print(output)
+
+    if(input("Do you want to continue? (Y/N) ").lower() == "n"):
+        break
